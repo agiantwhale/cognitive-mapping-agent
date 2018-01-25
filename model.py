2b6acfdec1502af75516f85665f2d9c18f042c7e
Il Jae Lee <iljae@umich.edu>
2018-01-25 15:42:58 -0500

    RNN mapper

--- model.py
+++ model.py
@@ -4,15 +4,17 @@ from tensorflow.contrib import slim
 
 class Model(object):
     @staticmethod
-    def _build_mapper(visual_input, previous_belief, egomotion, m={}, estimator=None):
-        def _estimate():
+    def _build_mapper(visual_input, egomotion, m={}, estimator=None):
+        ESTIMATE_SIZE = (16, 16)
+
+        def _estimate(image):
             with slim.arg_scope([slim.conv2d, slim.fully_connected],
                                 activation_fn=tf.nn.relu,
                                 padding='SAME',
                                 weights_initializer=tf.truncated_normal_initializer(stddev=0.01),
                                 weights_regularizer=slim.l2_regularizer(0.0005)):
                 with slim.arg_scope([slim.conv2d], stride=1, padding='SAME'):
-                    net = slim.conv2d(visual_input, 64, [5, 5])
+                    net = slim.conv2d(image, 64, [5, 5])
                     net = slim.conv2d(net, 128, [5, 5])
                     net = slim.conv2d(net, 256, [5, 5])
                     net = slim.fully_connected(net, 100)
@@ -21,8 +23,8 @@ class Model(object):
             m['temporal_belief'] = net
             return m['temporal_belief']
 
-        def _apply_egomotion():
-            translation, rotation = tf.unstack(egomotion)
+        def _apply_egomotion(belief, ego):
+            translation, rotation = tf.unstack(ego)
             # Transform relative to agent -- in other direction
             transform = tf.reshape(tf.stack([tf.cos(rotation),
                                              tf.sin(rotation),
@@ -31,7 +33,7 @@ class Model(object):
                                              tf.cos(rotation),
                                              tf.zeros(1, ),
                                              tf.zeros(1, )]), (8,))
-            m['warped_previous_belief'] = tf.contrib.image.transform(previous_belief, transform)
+            m['warped_previous_belief'] = tf.contrib.image.transform(belief, transform)
             return m['warped_previous_belief']
 
         def _warp(temp_belief, prev_belief):
@@ -44,8 +46,25 @@ class Model(object):
                                          current_confidence)
             return tf.stack([current_estimate, current_confidence], axis=2)
 
-        m['current_belief'] = _warp(_estimate() if estimator is None else estimator(),
-                                    _apply_egomotion())
+        class MapUnrollCell(tf.nn.rnn_cell.RNNCell):
+            @property
+            def state_size(self):
+                return tf.nn.rnn_cell.LSTMStateTuple(ESTIMATE_SIZE, ESTIMATE_SIZE)
+
+            @property
+            def output_size(self):
+                return ESTIMATE_SIZE
+
+            def __call__(self, inputs, state, scope=None):
+                image, ego = inputs
+
+                outputs = _warp(_estimate(image) if estimator is None else estimator(image),
+                                _apply_egomotion(state, ego))
+
+                return outputs, outputs
+
+        m['current_belief'] = tf.nn.dynamic_rnn(MapUnrollCell(), (visual_input, egomotion),
+                                                initial_state=tf.zeros(ESTIMATE_SIZE))
         return m['current_belief']
 
     @staticmethod
