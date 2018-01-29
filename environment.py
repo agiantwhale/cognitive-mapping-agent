9ad7314b572462432641e7fa77217ab9e03ea13e
Il Jae Lee <iljae@umich.edu>
2018-01-29 18:46:49 -0500

    Working on environment

--- environment.py
+++ environment.py
@@ -4,24 +4,24 @@ import deepmind_lab as dl
 import deepmind_lab_gym as dlg
 import multiprocdmlab as mpdmlab
 
+DEEPMIND_RUNFILES_PATH = os.path.dirname(inspect.getfile(dl))
+DEEPMIND_SOURCE_PATH = os.path.abspath(DEEPMIND_RUNFILES_PATH + "/.." * 5)
+dl.set_runfiles_path(DEEPMIND_RUNFILES_PATH)
 
-# from top_view_renderer import EntityMap
+
+def get_entity_layer_path(entity_layer_name):
+    global DEEPMIND_RUNFILES_PATH, DEEPMIND_SOURCE_PATH
+    mode, size, num = entity_layer_name.split("-")
+    path_format = "{}/assets/entityLayers/{}/{}/entityLayers/{}.entityLayer"
+    path = path_format.format(DEEPMIND_SOURCE_PATH, size, mode, num)
+
+    return mode, size, num, path
 
 
 def main():
-    building_name = "training-09x09-0127"
-    mode, size, num = building_name.split("-")
+    mapname = "training-09x09-0127"
 
-    deepmind_runfiles_path = os.path.dirname(inspect.getfile(dl))
-    deepmind_source_path = os.path.abspath(deepmind_runfiles_path + "/.." * 5)
-    mapstrings_path = "{}/assets/entityLayers/{}/{}/entityLayers/{}.entityLayer".format(deepmind_source_path,
-                                                                                        size,
-                                                                                        mode,
-                                                                                        num)
-
-    # entmap = EntityMap(mapstrings_path)
-
-    dl.set_runfiles_path(deepmind_runfiles_path)
+    mode, _, _, path = get_entity_layer_path(mapname)
 
     env = mpdmlab.MultiProcDeepmindLab(
         dlg.DeepmindLab
@@ -33,10 +33,9 @@ def main():
                , num_maps=1
                , withvariations=True
                , random_spawn_random_goal="True"
-               , entitydir=deepmind_runfiles_path
-               , chosen_map=building_name
-               , mapnames=building_name
-               , mapstrings=open(mapstrings_path).read()
+               , chosen_map=mapname
+               , mapnames=mapname
+               , mapstrings=open(path).read()
                , apple_prob=0.9
                , episode_length_seconds=5)
         , dlg.ActionMapperDiscrete
--- expert.py
+++ expert.py
@@ -3,12 +3,20 @@ from environment import get_entity_layer_path
 
 
 class Expert(object):
+    def _update_distance_to_goal(self):
+        pass
+
+    def _build_free_space_estimate(self):
+        pass
+
     def __init__(self, entity_layer):
         self._entity_layer_name = entity_layer
+        self._entity_map = EntityMap(get_entity_layer_path(entity_layer)[-1])
+        self._goal = None
+
+    def get_optimal_action(self, info):
+        raise NotImplementedError("optimal action is not yet implemented")
 
     @property
     def entity_layer_name(self):
         return self._entity_layer_name
-
-    def get_optimal_action(self, info):
-        raise NotImplementedError("optimal action is not yet implemented")
