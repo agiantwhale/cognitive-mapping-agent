8094b660c39e1309d3ab1251cb922a5e846159e8
Il Jae Lee <iljae@umich.edu>
2018-02-07 10:35:36 -0500

    Returned apple prob to 0.9

--- environment.py
+++ environment.py
@@ -37,7 +37,7 @@ def get_game_environment(mapname='training-09x09-0127', mode='training', multipr
                        , chosen_map=mapname
                        , mapnames=mapname
                        , mapstrings=mapstrings
-                       , apple_prob=0
+                       , apple_prob=0.9
                        , episode_length_seconds=5),
         'action_mapper': dlg.ActionMapperDiscrete,
         'additional_observation_types': ['GOAL.LOC', 'SPAWN.LOC', 'POSE', 'GOAL.FOUND']
