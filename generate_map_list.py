04a0688cf20b0f93b4e00d401674bb770f94a6c0
Il Jae Lee <agiantwhale@gmail.com>
2018-02-10 05:55:18 +0000

    New maps with err maps excluded

--- generate_map_list.py
+++ generate_map_list.py
@@ -4,15 +4,20 @@ def write_to_file(prefix, map_nums):
                          for map_num in map_nums))
 
 
+def generate_map_list(num, exclude_maps=frozenset()):
+    return [i for i in xrange(1, num + 1) if i not in exclude_maps]
+
+
 def main():
     static_maps = [127, 169, 246, 336, 445, 589, 691, 828, 844, 956]
+    exclude_maps = frozenset([2])
 
     write_to_file('static', static_maps)
     write_to_file('1', [1])
-    write_to_file('10', range(1, 11))
-    write_to_file('100', range(1, 101))
-    write_to_file('500', range(1, 501))
-    write_to_file('1000', range(1, 1001))
+    write_to_file('10', generate_map_list(10, exclude_maps))
+    write_to_file('100', generate_map_list(100, exclude_maps))
+    write_to_file('500', generate_map_list(500, exclude_maps))
+    write_to_file('1000', generate_map_list(1000, exclude_maps))
 
 
 if __name__ == '__main__':
